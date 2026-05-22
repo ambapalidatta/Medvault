285e5f2024bf22106fb7a2f6d893ebc6fe638448
Ambapali Datta <ambapali890@gmail.com>
2026-05-23 03:34:10 +0530

    Update EmailNotificationService.java

--- MedVault/MedVal/src/main/java/com/medval/service/EmailNotificationService.java
+++ MedVault/MedVal/src/main/java/com/medval/service/EmailNotificationService.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-// import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
 @Service
@@ -20,26 +19,23 @@ public class EmailNotificationService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async
     public void sendHtmlEmail(String to, String subject, String htmlBody) {
-        System.out.println("EMAIL METHOD HIT");
-        System.out.println("Sending email to: " + to);
-        System.out.println("From email: " + fromEmail);
-
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
-            helper.setText(htmlBody, true);
             helper.setTo(to);
             helper.setSubject(subject);
+            helper.setText(htmlBody, true);
             helper.setFrom(fromEmail);
 
             mailSender.send(mimeMessage);
 
-            System.out.println("✅ Email sent successfully to: " + to);
+            System.out.println("MAIL SENT SUCCESSFULLY");
 
-        } catch (MessagingException | MailException e) {
-            System.err.println("❌ Email sending failed to: " + to);
+        } catch (Exception e) {
+            System.out.println("MAIL FAILED");
             e.printStackTrace();
         }
     }
