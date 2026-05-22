d8374082284c111962d90793e8e6907d71b41ca5
Ambapali Datta <ambapali890@gmail.com>
2026-05-23 02:16:13 +0530

    Update EmailNotificationService.java

--- MedVault/MedVal/src/main/java/com/medval/service/EmailNotificationService.java
+++ MedVault/MedVal/src/main/java/com/medval/service/EmailNotificationService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
+// import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
@@ -20,7 +20,6 @@ public class EmailNotificationService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
-    @Async
     public void sendHtmlEmail(String to, String subject, String htmlBody) {
         System.out.println("EMAIL METHOD HIT");
         System.out.println("Sending email to: " + to);
